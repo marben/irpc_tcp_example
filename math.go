197695ce0fc63a3cbe7d56c6d1ad94fe38845dd1
Martin Benes <martinbenesh@gmail.com>
2025-10-25 13:11:46 +0200

    remove math service

--- math.go
+++
@@ -1,8 +0,0 @@
-package irpc_tcp_example
-
-// Math interface is the only thing needed to define our protocol
-// client and server code will be generated for us in math_irpc.go file using the command:
-// $ irpc math.go
-type Math interface {
-	Add(a, b int) (int, error)
-}
